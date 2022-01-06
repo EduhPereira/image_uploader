4ea82d1e5cd7f2822cfc8828233b35619c873bc9
Eduardo Pereira <augusto.pereira@protonmail.com>
2022-01-06 15:12:16 -0300

    fix: add cors

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const routes = require("./routes");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL);
 
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
